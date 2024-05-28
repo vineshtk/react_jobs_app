53efc2a08e62dbf84b9a9e794bf3cb9c632ff9f9
vineshtk <vineshtk10@gmail.com>
2024-05-28 11:22:35 +0530

    react basics + components + props

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+const App = () => {
+  return (
+
+    <>
+      <Navbar />
+      {/* <Hero title='This is title' subtitle='This is subtitle' /> */}
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      
+   
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+        >
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// const Card = ({children}) => {
+const Card = ({ children, bg='bg-grey-100' }) => {
+    return (
+        <div className={ `${bg} p-6 rounded-lg shadow-md`} >{children}</div>
+    )
+}
+
+export default Card
