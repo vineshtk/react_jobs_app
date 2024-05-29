dcc0e6bd59024c9966738c2fc976f204b92c4875
vineshtk <vineshtk10@gmail.com>
2024-05-29 10:08:07 +0530

    added routing, layouts and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-const App = () => {
-  return (
+import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
-    <>
-      <Navbar />
-      {/* <Hero title='This is title' subtitle='This is subtitle' /> */}
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      
-   
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
-    </>
-  )
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+    // <Route path='/about' element={<h1>My App</h1>} />
+
+  ))
+
+const App = () => {
+  // return (
+  //   <>
+  //     <Navbar />
+  //     {/* <Hero title='This is title' subtitle='This is subtitle' /> */}
+  //     <Hero />
+  //     <HomeCards />
+  //     <JobListings />
+  //     <ViewAllJobs />
+  //   </>
+
+  // )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 // const Card = ({children}) => {
-const Card = ({ children, bg='bg-grey-100' }) => {
+const Card = ({ children, bg = 'bg-grey-100' }) => {
     return (
-        <div className={ `${bg} p-6 rounded-lg shadow-md`} >{children}</div>
+        <div className={`${bg} p-6 rounded-lg shadow-md`} >
+            {children}
+        </div>
     )
 }
 
