ffdf260206096dfd7290096abbd62a9e4935090f
vineshtk <vineshtk10@gmail.com>
2024-06-05 23:09:44 +0530

    using data loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import HomeCards from './components/HomeCards';
+// import JobListings from './components/JobListings';
+// import ViewAllJobs from './components/ViewAllJobs';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
-import JobPage from './pages/JobPage';
+import JobPage, {jobLoader} from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} />
+      <Route path='/add-job' element={<AddJobPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
     // <Route path='/about' element={<h1>My App</h1>} />
